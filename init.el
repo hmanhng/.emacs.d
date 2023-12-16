f65781fa1fc9c57cba3b155379350957be21534f
Hmanhng <hmanhng@icloud.com>
2023-12-17 00:20:30 +0700

    init .emacs.d

--- init.el
+++ init.el
@@ -1,4 +1,6 @@
+(defvar my/emacs-dir user-emacs-directory "The path to the currently loaded .emacs.d directory. Must end with a slash.")
+
 (org-babel-load-file
  (expand-file-name
   "readme.org"
-  user-emacs-directory))
+  my/emacs-dir))
